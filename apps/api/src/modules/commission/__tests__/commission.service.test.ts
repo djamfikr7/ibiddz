bd163e265192945f081976f44623ad2c4d03a867
djamfikr7 <djamfikr7@gmail.com>
2026-03-31 19:42:16 +0100

    test: add commission service test stub

---
+++ apps/api/src/modules/commission/__tests__/commission.service.test.ts
@@ -0,0 +1,7 @@
+import { CommissionService } from '../commission.service';
+
+describe('CommissionService', () => {
+  it('should be defined', () => {
+    expect(CommissionService).toBeDefined();
+  });
+});
