46bac48e7402d25b5936140371b1f1a4a36f8bd7
Your Name <your_email@example.com>
2026-03-31 18:38:41 +0100

    test: add auction service integration test stub

---
+++ apps/api/src/modules/auction/__tests__/auction.service.test.ts
@@ -0,0 +1,7 @@
+import { AuctionService } from '../auction.service';
+
+describe('AuctionService - Integration', () => {
+  it('should be defined', () => {
+    expect(AuctionService).toBeDefined();
+  });
+});
